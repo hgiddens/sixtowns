2716cef7c62208f3c46980209116557db0bc2168
Huw Giddens <huw@robotines.co.nz>
2010-09-15 19:01:36 +1200

    Use utility libraries.

--- sixtowns.lisp
+++ sixtowns.lisp
@@ -1,16 +1,15 @@
 (cl:defpackage :sixtowns
-  (:use :cl))
+  (:use :cl :anaphora :metabang.utilities))
 (cl:in-package :sixtowns)
 
 (defvar *connection* nil)
 
 (defun connect ()
   (assert (null *connection*))
-  (let ((connection (xmpp:connect-tls :hostname *xmpp-host*)))
-    (xmpp:auth connection *xmpp-user* *xmpp-pass* *xmpp-resource* :mechanism :sasl-plain)
-    (setf *connection* connection)))
+  (setf *connection* (aprog1 (xmpp:connect-tls :hostname *xmpp-host*)
+                       (xmpp:auth it *xmpp-user* *xmpp-pass* *xmpp-resource* :mechanism :sasl-plain))))
 
 (defun disconnect ()
   (assert (not (null *connection*)))
   (xmpp:disconnect *connection*)
-  (setf *connection* nil))
+  (nilf *connection*))
