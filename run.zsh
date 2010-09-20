e810fdda256abab308d64936f2e08d1be8b8bec8
Huw Giddens <huw@robotines.co.nz>
2010-09-21 02:14:51 +1200

    Rename private configuration file.

--- run.zsh
+++ run.zsh
@@ -4,7 +4,7 @@ ccl -e '(require :asdf)' \
     -e '(push (merge-pathnames "Source/clbuild/systems/" (user-homedir-pathname)) asdf:*central-registry*)' \
     -e "(dolist (system '(:cl-xmpp-tls :babel :cl-oauth :cxml :cl-containers :anaphora :metatilities :xpath :split-sequence :bordeaux-threads)) (asdf:oos 'asdf:load-op system :verbose nil))" \
     -e '(load "package.lisp")' \
-    -e '(load "secret.lisp")' \
+    -e '(load "config.lisp")' \
     -e '(load "sixtowns.lisp")' \
     -e '(sixtowns::start)'
 
